9ac2bb191fc8ae096be7a49e735d2c5a4f6a128a
Aktar Zaman <aktarzaman@berkeley.edu>
2020-04-19 02:31:31 -0400

    Have to figure out how to structure the app

--- Test_App_5/AppDelegate.swift
+++ Test_App_5/AppDelegate.swift
@@ -41,20 +41,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 }
 
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        completionHandler([.alert, .sound])
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        print("Hello hello")
-    }
-    
-}
 
 
 
