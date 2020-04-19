d9dab3bda28b9fadf8bfb4997133459284ebd322
Aktar Zaman <aktarzaman@berkeley.edu>
2020-04-19 02:16:15 -0400

    first commit

--- Test_App_5/ViewController.swift
+++ Test_App_5/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
+    
+//    var appDelegate = UIApplication.shared.delegate as? AppDelegate
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+    
+    @IBAction func scheduleNotification(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Lock the phone to hear the notification", message: "The notification will play in 10 secs", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.delegate = self
+        notificationCenter.removeAllDeliveredNotifications()
+                
+                
+        let content = UNMutableNotificationContent()
+        content.title = "My name is Zamy"
+        content.body = "This is example how to create "
+        content.badge = 1
+        content.sound = UNNotificationSound.default
+        content.categoryIdentifier = "confusion"
+                
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+        let requestIdentifier = "Local Notification"
+                
+        let request = UNNotificationRequest(identifier: requestIdentifier, content: content, trigger: trigger)
+                
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+        
+    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        UNUserNotificationCenter.current().delegate = self
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+    
+}
+
+extension ViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: (UNNotificationPresentationOptions) -> Void) {
+    // some other way of handling notification
+    completionHandler([.alert, .sound])
+    }
+    // I am really confused about this thing and i need help understanding these two functions,
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: () -> Void) {
+        print("Will work")
     }
 
-
 }
+
 
